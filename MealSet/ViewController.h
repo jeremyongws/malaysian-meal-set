9762a0314d0c20355332ffc3bd88a23381ea4a5f
Jeremy Ong <jeremy.ongws@gmail.com>
2016-04-06 10:05:53 +0800

    Initial Commit

---
+++ MealSet/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MealSet
+//
+//  Created by Jeremy Ong on 06/04/2016.
+//  Copyright © 2016 Jeremy Ong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MealSet/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MealSet
+//
+//  Created by Jeremy Ong on 06/04/2016.
+//  Copyright © 2016 Jeremy Ong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+	[super didReceiveMemoryWarning];
+	// Dispose of any resources that can be recreated.
+}
+
+@end
